ff0b29876a6fceba0ef0f0129b1cb6ad34805ac3
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-11-11 13:55:00 +0530

    Task 10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useCallback } from "react";
-
 import MoviesList from "./components/MoviesList";
+import Form from "./components/Form";
 import "./App.css";
 
 function App() {
@@ -73,6 +73,9 @@ function App() {
 
     return (
         <React.Fragment>
+            <section>
+                <Form />
+            </section>
             <section>
                 <button onClick={fetchMovieHandler}>Fetch Movies</button>
             </section>
